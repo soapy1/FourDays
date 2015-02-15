409faae5e4e07a8057c149e6760bdb2b67058068
Sophia <scastellarin95@gmail.com>
2015-02-15 11:46:44 -0500

    added stuff....... current state

--- lfp1.py
+++ lfp1.py
@@ -5,32 +5,68 @@ from Person import Person
 
 game_path = {
     'cond':'You need to wake up to get to work',
-     'act': [
+    'act': [
         {
             'opt':1, 
+            'outcome': None,
             'val': 'wake up at 7am', 
             'pre':[], 
             'next': 
                 {
                     'cond': 'you need to get to work',
-                    'act': [
-                        {
+                    'act': [{
                             'opt':1,
+                            'outcome': 'Manager is mad. You get beat.', 
                             'val':'walk',
                             'pre':[],
-                            'next':{}
+                            'next':{
+                                'cond': 'challenge',
+                                'word': 'sorry',
+                                'times': 10,
+                                'next': 
+                                    {
+                                        'cond': 'other thing',
+                                        'act': [
+                                            {
+                                                'opt':1,
+                                                'outcome': None,
+                                                'val':'walk',
+                                                'pre':[],
+                                                'next':{
+                                                    'cond': '',
+                                                    'act': []
+                                                }
+                                            },
+                                            {
+                                                'opt':2,
+                                                'outcome': None,
+                                                'val':'run',
+                                                'pre':[],
+                                                'next':{
+                                                    'cond': '',
+                                                    'act': []
+                                                }
+                                            }
+                                        ]
+                                    }
+                            }
                         },
                         {
                             'opt':2,
+                            'outcome': None,
                             'val':'run',
                             'pre':[],
-                            'next':{}
+                            'next':{
+                                'cond': '',
+                                'act': []
+                            }
                         }
                     ]
                 }
         },
         {
             'opt':2, 
+            'outcome': None,
             'val': 'wake up at 6am', 
             'pre':[], 
             'next': 
@@ -39,15 +75,23 @@ game_path = {
                     'act': [
                         {
                             'opt':1,
+                            'outcome': None,
                             'val':'walk',
                             'pre':[],
-                            'next':{}
+                            'next':{
+                                'cond': '',
+                                'act': []
+                            }
                         },
                         {
                             'opt':2,
+                            'outcome': None,
                             'val':'run',
                             'pre':[],
-                            'next':{}
+                            'next':{
+                                'cond': '',
+                                'act': []
+                            }
                         }
                     ]
                 }
@@ -90,34 +134,47 @@ def print_stats(p):
     print(" - have {0} dollars".format(p.get_money()))
     print(" - have {0} health\n".format(p.get_health()))
 
+def story_intro():
+    print("\n")
+    print("It's a scoldering hot day!\n ")
 
 def main():
-
     current_pos = game_path
+    
+#    story_intro()
     player = intro2()
     print_stats(player)
     game = True
     while game == True:
-        print('\n' + current_pos['cond'])
-        for i in current_pos['act']:
-            print(" - [{0}] {1}".format(i['opt'], i['val']))
-
-
-        opt = input("\nwhat do you do? ").lower()
-
-        if opt == "quit":
-            game = False
-        elif opt in ['1','2','3','4','5']:
-            for i in current_pos['act']:
-                if (int(opt) == i['opt']):
-                    current_pos = i['next']
-        elif opt == "work":
-            if challenge('work', 10):
-                print("good job for today")
+        if (current_pos['cond'] == "challenge"):
+            print("CHALLENGE: type {0} {1} time".format(current_pos['word'], current_pos['times']))
+            if challenge(current_pos['word'], current_pos['times']):
+                print("good job")
             else:
                 print("you fail")
-        else:
-            print(("you said {0}").format(opt))
+            current_pos = current_pos['next']
+        else: 
+            print('\n' + current_pos['cond'])
+            for i in current_pos['act']:
+                print(" - [{0}] {1}".format(i['opt'], i['val']))
+
+
+            opt = input("\nwhat do you do? ").lower()
+
+            if opt == "q":
+                game = False
+            elif opt in ['1','2','3','4','5']:
+                for i in current_pos['act']:
+                    if (int(opt) == i['opt']):
+                        print(i['outcome'])
+                        current_pos = i['next']
+            elif opt == "work":
+                if challenge('work', 10):
+                    print("good job for today")
+                else:
+                    print("you fail")
+            else:
+                print(("you said {0}").format(opt))
 
 if __name__ == "__main__":
     main()
